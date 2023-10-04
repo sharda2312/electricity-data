b0528b69bedc12adaf29659cbaba6d009f03c17e
Shubham Kumar Mahato <125397228+backpackerdeveloper@users.noreply.github.com>
2023-10-04 19:03:46 +0530

    Update app.py
    Changing entire code to test diffrent api

--- app.py
+++ app.py
@@ -1,77 +1,64 @@
 import json
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from flask import Flask, request, jsonify
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__)
 
-@app.route('/get', methods=['POST'])
-def get_energy_bill():
-    try:
-        # Configure Chrome to run in headless mode
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
+@app.route('/scrape_attendance', methods=['POST'])
+def scrape_attendance():
+    # Get JSON input from the POST request
+    data = request.get_json()
 
-        # Get consumer_no and office_address_value from the request JSON
-        data = request.json
-        consumer_no = data.get("consumer_no")
-        office_address_value = data.get("office_address_value")
+    # Extract username and password from the JSON input
+    username = data.get('username')
+    password = data.get('password')
 
-        # Create a Selenium webdriver with headless mode
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get("https://www.jseb.co.in/WSS/WSSUI/frmpwlEnergyBillPayment.aspx")
+    # Configure Chrome to run in headless mode
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")  # For Linux systems
 
-        # Wait for the page to load and the JavaScript to execute
-        driver.implicitly_wait(10)
+    # Create a Selenium webdriver with headless mode
+    driver = webdriver.Chrome(options=chrome_options)
+    
+    # Log in to the website
+    login_url = "https://ums.paruluniversity.ac.in/Login.aspx"
+    driver.get(login_url)
 
-        # Select the "Consumer No." radio button
-        consumer_radio_button = driver.find_element(By.ID, "ctl00_cphpwl_rbtAccountNumber")
-        consumer_radio_button.click()
+    username_input = driver.find_element(By.ID, "txtUsername")
+    password_input = driver.find_element(By.ID, "txtPassword")
+    login_button = driver.find_element(By.ID, "btnLogin")
 
-        # Enter the Consumer No. in the input box
-        consumer_input = driver.find_element(By.ID, "ctl00_cphpwl_txtConsumerNumber")
-        consumer_input.send_keys(consumer_no)
+    username_input.send_keys(username)
+    password_input.send_keys(password)
+    login_button.click()
 
-        # Select the Office Address from the dropdown menu
-        office_address_dropdown = driver.find_element(By.ID, "ctl00_cphpwl_ddlOfficeAddress")
-        for option in office_address_dropdown.find_elements(By.TAG_NAME, 'option'):
-            if option.get_attribute("value") == office_address_value:
-                option.click()
-                break
+    # Wait for the page to load
+    driver.implicitly_wait(10)  # Use implicit wait instead of time.sleep to wait for elements
 
-        # Click the "Get Bill Details" button using JavaScript
-        driver.execute_script("arguments[0].click();", driver.find_element(By.ID, "ctl00_cphpwl_btnGetBillDetail"))
+    # Visit the attendance page
+    attendance_url = "https://ums.paruluniversity.ac.in/StudentPanel/TTM_Attendance/TTM_Attendance_StudentAttendance.aspx"
+    driver.get(attendance_url)
 
-        # Wait for the page to redirect and load
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "ctl00_cphpwl_lblConsumerName")))
+    # Wait for the attendance data to load (you may need to adjust the wait time)
+    driver.implicitly_wait(10)
 
-        # Extract data from the redirected page
-        page_source = driver.page_source
-        soup = BeautifulSoup(page_source, "html.parser")
+    # Parse the attendance data
+    attendance_element = driver.find_element(By.ID, "ctl00_cphPageContent_lblPresentPCTCount")
+    attendance = attendance_element.text
 
-        # Find the required data and store it in a dictionary
-        data = {
-            "Consumer Name": soup.find("span", {"id": "ctl00_cphpwl_lblConsumerName"}).text.strip(),
-            "Bill No.": soup.find("span", {"id": "ctl00_cphpwl_lblBillNo"}).text.strip(),
-            "KNo": soup.find("span", {"id": "ctl00_cphpwl_lblKNo1"}).text.strip(),
-            "Bill Month": soup.find("span", {"id": "ctl00_cphpwl_lblBillMonth"}).text.strip(),
-            "Due Date": soup.find("span", {"id": "ctl00_cphpwl_lblDueDate"}).text.strip(),
-            "Bill Amount (Rs.)": soup.find("span", {"id": "ctl00_cphpwl_lblBilledAmount"}).text.strip(),
-            "Amount Payable (Rs.)": soup.find("span", {"id": "ctl00_cphpwl_lblAmountPayble"}).text.strip(),
-        }
+    # Create a JSON response
+    attendance_data = {
+        "Attendance": attendance
+    }
 
-        # Close the browser
-        driver.quit()
+    # Close the browser
+    driver.quit()
 
-        # Return the JSON response
-        return jsonify({"result": data})
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    return jsonify(attendance_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
